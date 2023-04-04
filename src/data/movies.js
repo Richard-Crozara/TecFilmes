4350326408e8b8a8e99c37204102baf391ead134
RichardCroz <100240123+Richard-Crozara@users.noreply.github.com>
2023-04-04 12:10:10 -0300

    Add files via upload

---
+++ src/data/movies.js
@@ -0,0 +1,40 @@
+const DATA =
+[
+    {
+        id: '01',
+        nome: 'Adão Negro',
+        nota: '8.8',
+        imagem: 'adao.jpg'
+    },
+    {
+        id: '02',
+        nome: 'Avatar',
+        nota: '8.3',
+        imagem: 'Avatar.jpg'    
+    },
+    {
+        id: '03',
+        nome: 'Gato de Botas 2',
+        nota: '9.7',
+        imagem: 'Gato.jpg'    
+    },
+    {
+        id: '04',
+        nome: 'Pantera Negra',
+        nota: '8.2',
+        imagem: 'Pantera.jpg'    
+    },
+    {
+        id: '05',
+        nome: 'Homem-Aranha: Sem Volta para Casa',
+        nota: '8.2',
+        imagem: 'Aranha.jpg'    
+    },
+    {
+        id: '06',
+        nome: 'Homem-Aranha: Sem Volta para Casa',
+        nota: '8.2',
+        imagem: 'Aranha.jpg'    
+    },
+    
+]
