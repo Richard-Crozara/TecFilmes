b04b982e37dc5c775cabd62b42e507ef4198eaf8
RichardCroz <100240123+Richard-Crozara@users.noreply.github.com>
2023-04-11 11:12:08 -0300

    Update movies.js

--- src/data/movies.js
+++ src/data/movies.js
@@ -4,60 +4,60 @@ const DATA =
         id: '01',
         nome: 'Adão Negro',
         nota: '8.8',
-        imagem: 'adao.jpg'
+        imagem: '1.jpg'
     },
     {
         id: '02',
         nome: 'Avatar',
         nota: '8.3',
-        imagem: 'Avatar.jpg'    
+        imagem: '2.jpg'    
     },
     {
         id: '03',
         nome: 'Gato de Botas 2',
         nota: '9.7',
-        imagem: 'Gato.jpg'    
+        imagem: '3.jpg'    
     },
     {
         id: '04',
         nome: 'Pantera Negra',
         nota: '8.2',
-        imagem: 'Pantera.jpg'    
+        imagem: '4.jpg'    
     },
     {
         id: '05',
         nome: 'Homem-Aranha: Sem Volta para Casa',
-        nota: '8.2',
-        imagem: 'Aranha.jpg'    
+        nota: '8.4',
+        imagem: '5.jpg'    
     },
     {
         id: '06',
         nome: 'Super Mario Bros',
         nota: '8.0',
-        imagem: 'Mario.jpg'    
+        imagem: '6.jpg'    
     },
     {
         id: '07',
         nome: 'One Piece Red',
         nota: '9.6',
-        imagem: 'One Piece.jpg'    
+        imagem: '7.jpg'    
     },
     {
         id: '08',
         nome: 'Doutor Estranho no Multiverso da Loucura',
         nota: '9.0',
-        imagem: 'Doutor Estranho.jpg'    
+        imagem: '8.jpg'    
     },
     {
         id: '09',
         nome: 'Harry Potter',
         nota: '9.8',
-        imagem: 'Harry Potter.jpg'    
+        imagem: '9.jpg'    
     },
     {
         id: '10',
         nome: 'Shazam! Fúria dos Deuses',
         nota: '7.0',
-        imagem: 'Shazam.jpg'    
+        imagem: '10.jpg'    
     }
 ]
